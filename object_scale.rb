ffa9e37ab3986a32c1a754bd7f8d426744bbefe5
Eneroth3 <eneroth3@gmail.com>
2020-10-09 21:53:39 +0200

    Refactor

--- object_scale.rb
+++ object_scale.rb
@@ -24,25 +24,21 @@ def remove_scaling(transformation)
 end
 
 def scaling(transformation, bounds)
-  x_scale = Geom::Vector3d.new(transformation.to_a.values_at(0..2)).length
-  y_scale = Geom::Vector3d.new(transformation.to_a.values_at(4..6)).length
-  z_scale = Geom::Vector3d.new(transformation.to_a.values_at(8..10)).length
+  scales = [
+    Geom::Vector3d.new(transformation.to_a.values_at(0..2)).length,
+    Geom::Vector3d.new(transformation.to_a.values_at(4..6)).length,
+    Geom::Vector3d.new(transformation.to_a.values_at(8..10)).length
+  ]
   
-  # In case instance is 2D, native SketchUp scale tool will only affect two
-  # axes. Ignore the scaling in he flat axis.
-  flat = true
-  if bounds.width == 0
-    x_scale = 1
-  elsif bounds.height == 0 # "height" = depth (Y)
-    y_scale = 1
-  elsif bounds.depth == 0 # "depth" = height (z)
-    z_scale = 1
-  else
-    flat = false
-  end
-  product = x_scale * y_scale * z_scale
+  # Native Scale tool only scale flat objects in their plane.
+  # If object is flat, ignore third axis when calculating scale.
+  # Ignore hypothetical axial objects.
+  # TODO: Can flatness be expressed as bitmask and axial objects be supported?
+  flat = zero_axis(bounds)
+  scales[flat] = 1 if flat
   
   # TODO: Honor last element
+  product = scales.inject(:*)
   flat ? Math.sqrt(product) : Math.cbrt(product)
 end
 
@@ -54,57 +50,20 @@ def uniform?(transformation, bounds)
   x_scale == y_scale && x_scale == z_scale
 end
 
-def flat?(bounds)
-  
-end
-
-
-
-
-
-# Extract scaling from transformation.
+# Get index to the bounds is flat in, if any.
 #
-# @param transformation [Geom::Transformation]
+# @param bounds [Geom::BoundingBox]
 #
-# @return [Float]
-def scaling(transformation)
-  xscale = Geom::Vector3d.new(transformation.to_a.values_at(0..2)).length
-  yscale = Geom::Vector3d.new(transformation.to_a.values_at(4..6)).length
-  zscale = Geom::Vector3d.new(transformation.to_a.values_at(8..10)).length
-  
-  # By default Scale tool scales flat objects only in two axes,
-  # meaning we need to ignore any axis scaling not matching the other two.
-  return x_scale.to_f if xscale == y_scale
-  return x_scale.to_f if xscale == z_scale
-  return y_scale.to_f if yscale == z_scale
-  
-  # What if object is stretched in just one axis?
-  
-  Math.cbrt(x_scale * y_scale * z_scale)
-end
-
-def scaling(transformation)
-  xaxis = Geom::Vector3d.new(transformation.to_a.values_at(0..2))
-  yaxis = Geom::Vector3d.new(transformation.to_a.values_at(4..6))
-  zaxis = Geom::Vector3d.new(transformation.to_a.values_at(8..10))
-  
-  xaxis % (yaxis * zaxis) / transformation.to_a[15]
-end
-
-
-
-# Get uniform scaling from component.
-#
-# @return [Float, nil]
-def scaling(transformation)
-  xaxis = Geom::Vector3d.new(transformation.to_a.values_at(0..2))
-  yaxis = Geom::Vector3d.new(transformation.to_a.values_at(4..6))
-  zaxis = Geom::Vector3d.new(transformation.to_a.values_at(8..10))
-  return unless xaxis.length == yaxis.length
-  return unless xaxis.length == zaxis.length
-  
-  # Last element in transformation may be used for uniform scaling inverted.
-  xaxis.length / transformation.to_a[15]
+# @return [Integer, nil]
+#   0 = x, 1 = y, 2 = z, nil = bounds is not flat.
+def zero_axis(bounds)
+  if bounds.width == 0
+    0
+  elsif bounds.height == 0 # "height" = depth (Y)
+    1
+  elsif bounds.depth == 0 # "depth" = height (z)
+    2
+  end
 end
 
 entity = Sketchup.active_model.selection.first
